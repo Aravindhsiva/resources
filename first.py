e0df99cb0576c364cce443cd7a11ea3a90c52354
Aravindh Siva <adamaravindh@gmail.com>
2018-09-24 15:50:09 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,27 @@
+x=1
+y=2.8
+z="Aravindhisawesome"
+print(x+y)
+if x < y:
+    print("x is lesser than y")
+else:
+    print("x is greater than y")
+print (z[10:17])
+#list
+friends = ["Aravindh", "Arun", "Ajay"]
+#for_loop
+for x in friends:
+  print(x) 
+#tuple
+tuples = ("first", "second", "third")
+print(tuples[1])
+#dictionary
+dictionary =	{
+  "name": "Aravindh",
+  "Age": 20,
+  "Year": "Third"
+}
+print (dictionary["name"])
+
+dictionary["name"] = "Siva"
+print (dictionary)
